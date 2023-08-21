f42089b56510b9d7478eb2ddf2e8b20056709b29
Edson Greig <edson2495@hotmail.com>
2023-08-21 16:18:30 -0500

    setChild and just one function for rotation(left and right).

--- avl.h
+++ avl.h
@@ -8,22 +8,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// template <typename T>
-// class NodeAVL : public NodeBinaryTree
-// {
-// public:
-//   typedef T         Type;
-// private:
-//   using Parent = NodeBinaryTree<T> Node;
-//   public:
-//     size_t m_depth; 
-//   public:
-//     NodeAVL(Node *pParent, T data, Node *p0 = nullptr, Node *p1 = nullptr) 
-//         : Parent(pParent, p0, p1)
-//     {}
-
-// };
-
 // TODO: AVL
 template <typename Traits>
 class CAVL: public BinaryTree<Traits>{
@@ -49,62 +33,50 @@ class CAVL: public BinaryTree<Traits>{
       return height(pNode->getChild(0)) - height(pNode->getChild(1));
     }
 
-    Node* leftRotation(Node* pCurrentRoot, Node* pParent){
-      Node* pNewRoot = pCurrentRoot->getChild(1); //new root of the subtree
-      pCurrentRoot->setpChild(pNewRoot->getChild(0),1);
-      if(pNewRoot->getChild(0)){pNewRoot->getChild(0)->setParent(pCurrentRoot);}
-      pNewRoot->setpChild(pCurrentRoot,0);
-      pCurrentRoot->setParent(pNewRoot);
-      
+    void setChild(Node* ptrParent, Node* ptrChild, size_t numberChild){
+      ptrParent->setpChild(ptrChild,numberChild);
+      if(ptrChild){ptrChild->setParent(ptrParent);}//in case child is null
+    }
+
+  Node* rotation(Node* pCurrentRoot, Node* pParent, string type){
+      Node* pNewRoot = pCurrentRoot->getChild(type=="left"?1:0); //new root of the subtree
+      setChild(pCurrentRoot,pNewRoot->getChild(type=="left"?0:1),type=="left"?1:0);
+      setChild(pNewRoot,pCurrentRoot,type=="left"?0:1);
       pNewRoot->setParent(pParent);
       pCurrentRoot->setHeight( max( height(pCurrentRoot->getChild(0)) , height(pCurrentRoot->getChild(1)) ) + 1 );
       pNewRoot->setHeight( max( height(pNewRoot->getChild(0)) , height(pNewRoot->getChild(1)) ) + 1 );
       return pNewRoot;
-    }
+  }
 
-    Node* rightRotation(Node* pCurrentRoot, Node* pParent){
-      Node* pNewRoot = pCurrentRoot->getChild(0); //new root of the subtree
-      pCurrentRoot->setpChild(pNewRoot->getChild(1),0);
-      if(pNewRoot->getChild(1)){pNewRoot->getChild(1)->setParent(pCurrentRoot);}
-      pNewRoot->setpChild(pCurrentRoot,1);
-      pCurrentRoot->setParent(pNewRoot);
-      
-      pNewRoot->setParent(pParent);
-      pCurrentRoot->setHeight( max( height(pCurrentRoot->getChild(0)) , height(pCurrentRoot->getChild(1)) ) + 1 );
-      pNewRoot->setHeight( max( height(pNewRoot->getChild(0)) , height(pNewRoot->getChild(1)) ) + 1 );
-      return pNewRoot;
-    }
+  void balance(Node* pNode, Node *& pNewRoot){
+    pNode->setHeight( max( height(pNode->getChild(0)) , height(pNode->getChild(1)) ) + 1 );
+    int bF = balanceFactor(pNode);
+    Node* pParent = pNode->getParent();
 
-
-    void balance(Node* pNode, Node *& pNewRoot){
-      pNode->setHeight( max( height(pNode->getChild(0)) , height(pNode->getChild(1)) ) + 1 );
-      int bF = balanceFactor(pNode);
-      Node* pParent = pNode->getParent();
-
-      if( bF > 1 || bF < -1 ){
-        size_t numberChild;
-        if(pParent){numberChild = (pParent->getChild(0)==pNode?0:1);}
-        if(bF == -2){//left rotation cases
-          if(balanceFactor(pNode->getChild(1)) == -1){//left roration
-            pNewRoot = leftRotation(pNode,pParent);
-          }else if(balanceFactor(pNode->getChild(1)) == 1){//right-left roration
-            pNode->setpChild(rightRotation(pNode->getChild(1),pNode),1);
-            pNewRoot = leftRotation(pNode,pParent);
-          }
+    if( bF > 1 || bF < -1 ){
+      size_t numberChild;
+      if(pParent){numberChild = (pParent->getChild(0)==pNode?0:1);}
+      if(bF == -2){//left rotation cases
+        if(balanceFactor(pNode->getChild(1)) == -1){//left roration
+          pNewRoot = rotation(pNode,pParent,"left");
+        }else if(balanceFactor(pNode->getChild(1)) == 1){//right-left roration
+          pNode->setpChild(rotation(pNode->getChild(1),pNode,"right"),1);
+          pNewRoot = rotation(pNode,pParent,"left");
         }
-        if(bF == 2){//right rotation cases
-          if(balanceFactor(pNode->getChild(0)) == 1){//right roration
-            pNewRoot = rightRotation(pNode,pParent);
-          }else if(balanceFactor(pNode->getChild(0)) == -1){//left-right roration
-            pNode->setpChild(leftRotation(pNode->getChild(0),pNode),0);
-            pNewRoot = rightRotation(pNode,pParent);
-          }
-        }
-        if(pParent){pParent->setpChild(pNewRoot,numberChild);}
-        else{this->m_pRoot = pNewRoot;}
       }
-      if(pParent){balance(pParent,pNewRoot);}
+      if(bF == 2){//right rotation cases
+        if(balanceFactor(pNode->getChild(0)) == 1){//right roration
+          pNewRoot = rotation(pNode,pParent,"right");
+        }else if(balanceFactor(pNode->getChild(0)) == -1){//left-right roration
+          pNode->setpChild(rotation(pNode->getChild(0),pNode,"left"),0);
+          pNewRoot = rotation(pNode,pParent,"right");
+        }
+      }
+      if(pParent){pParent->setpChild(pNewRoot,numberChild);}
+      else{this->m_pRoot = pNewRoot;}
     }
+    if(pParent){balance(pParent,pNewRoot);}
+  }
 
   public: 
     // TODO: insert must receive two paramaters: elem and LinkedValueType value
